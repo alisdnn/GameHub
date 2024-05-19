a15c3efbde7d8362eb2e46bdb607db3994d6b97a
hojat <hojat72elect@gmail.com>
2024-05-19 03:24:59 -0400

    Ticket_25 : Changed the package name of the app to "ca.hojat.gamehub" - part 3

---
+++ app/src/test/java/ca/hojat/gamehub/core/domain/FakeData.kt
@@ -0,0 +1,5 @@
+package ca.hojat.gamehub.core.domain
+
+import ca.hojat.gamehub.core.domain.games.common.RefreshUseCaseParams
+
+internal val REFRESH_GAMES_USE_CASE_PARAMS = RefreshUseCaseParams()
---
+++ app/src/test/java/ca/hojat/gamehub/feature_settings/FakeData.kt
@@ -0,0 +1,10 @@
+package ca.hojat.gamehub.feature_settings
+
+import ca.hojat.gamehub.feature_settings.domain.DomainLanguage
+import ca.hojat.gamehub.feature_settings.domain.DomainSettings
+import ca.hojat.gamehub.feature_settings.domain.DomainTheme
+
+internal val DOMAIN_SETTINGS = DomainSettings(
+    theme = DomainTheme.DARK,
+    language = DomainLanguage.PERSIAN,
+)
